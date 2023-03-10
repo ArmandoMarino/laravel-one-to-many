5d37fc02127fe44c24d252457cbe23e19e60d27f
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-10 23:25:29 +0100

    project controller

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace Database\Seeders;
-
+// MODELS
 use App\Models\Project;
+use App\Models\Type;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
 use illuminate\support\Str;
 
 class ProjectSeeder extends Seeder
@@ -15,9 +18,14 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+        // Prende l'id dal controller type seleziona  l'id e poi lo trsaforma in un array (di id)
+        $type_ids = Type::select('id')->pluck('id')->toArray();
+
         for ($i = 0; $i < 20; $i++) {
             $project = new Project();
 
+            // Assegnamo un id random preso dal $type_ids sopra e lo seediamo
+            $project->type_id = Arr::random($type_ids);
             $project->title = $faker->text(20);
             $project->description = $faker->paragraphs(15, true);
             // $project->image = $faker->imageUrl(250, 250);
