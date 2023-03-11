48241cb94257e8a8914e0a6ca19e54e79e359aa2
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-11 12:36:30 +0100

    color rendering on index types

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -27,7 +27,7 @@
         <tr>
             <th scope="row">{{$type->id }}</th>
             <td>{{$type->label }}</td>
-            <td>{{$type->color }}</td>
+            <td style="background-color : {{$type->color}}"></td>
             <td>{{$type->created_at }}</td>
             <td>{{$type->updated_at }}</td>
             <td class="d-flex">
