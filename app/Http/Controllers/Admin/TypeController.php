8f8dd0cdc4ee6f990f75c8c308db8c84d0a78e6b
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-11 11:13:47 +0100

    type controller cruds

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Type;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class TypeController extends Controller
 {
@@ -22,7 +23,8 @@ class TypeController extends Controller
      */
     public function create()
     {
-        //
+        $type = new Type();
+        return view('admin.types.create', compact('type'));
     }
 
     /**
@@ -30,38 +32,72 @@ class TypeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'label' => 'required|string|unique:types|max:15',
+            'color' => 'nullable|string|size:7'
+        ], [
+            'label.required' => 'Type select is required',
+            'label.max' => 'The type can have maximum of 15 characters',
+            'label.unique' => 'This Type name is already taken',
+            'color.size' => 'The color must be a hexadecimal code with a pound sign.',
+        ]);
+
+        $data = $request->all();
+
+        $type = new Type();
+
+        $type->fill($data);
+
+        $type->save();
+
+        return to_route('admin.types.show', $type->id)->with('type', 'success')->with('message', "$type->label created successfully");
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Type $type)
     {
-        //
+        return view('admin.types.show', compact('type'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Type $type)
     {
-        //
+        return view('admin.types.edit', compact('type'))
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Type $type)
     {
-        //
+        $request->validate([
+            'label' => ['required','string',Rule::unique('types')->ignore($type->id),'max:15'],
+            'color' => 'nullable|string|size:7'
+        ], [
+            'label.required' => 'Type select is required',
+            'label.max' => 'The type can have maximum of 15 characters',
+            'label.unique' => 'This Type name is already taken',
+            'color.size' => 'The color must be a hexadecimal code with a pound sign.',
+        ]);
+
+        $data = $request->all();
+
+        $type->update($data);
+
+        return to_route('admin.types.show', $type->id)->with('type', 'success')->with('message', "$type->label updated successfully");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Type $type)
     {
-        //
+        $type->delete();
+
+        return to_route('admin.types.index')->with('type', 'success')->with('message', "$type->label deleted successfully");
     }
 }
